0ed3575e8339dd84451e036e82051eb45e418ae0
puc_dong <1417766861@qq.com>
2019-04-10 13:41:09 +0800

    判断数字回文

---
+++ 判断一个整数是否是回文数.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        x = str(x)
+        if x == x[::-1]:
+            return True
+        return False
+
+s = Solution().isPalindrome(-121)
+print(s)
