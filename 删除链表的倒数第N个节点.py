2754a827e933735d649bcdca36231ad888ded12e
puc_dong <1417766861@qq.com>
2019-04-12 11:28:29 +0800

    删除链表的倒数第N个节点

--- 删除链表的倒数第N个节点.py
+++ 删除链表的倒数第N个节点.py
@@ -1,31 +1,38 @@
-
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Mar 17 16:35:29 2018
-
-@author: Saul
-"""
-
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    def __init__(self, x,next=None):
+        self.val = x
+        self.next = next
 
-class Solution:
+class Solution(object):
     def removeNthFromEnd(self, head, n):
         """
         :type head: ListNode
         :type n: int
         :rtype: ListNode
         """
-        dummy = ListNode(0)
-        dummy.next = head
-        p1 = p2 = dummy
-        for i in range(n):
-            p1 = p1.next
-        while p1.next:
-            p1 = p1.next
-            p2 = p2.next
-        p2.next = p2.next.next
-        return dummy.next
+        mynode = ListNode(0)
+        p = head
+        mynode.next = p
+        length = 0
+        while head:
+            length+=1
+            head = head.next
+
+        if n == length:
+            return p.next
+
+        for x in range(1,length-n+1):
+            if x == length-n:
+                p.next = p.next.next
+            p = p.next
+        return mynode.next
+
+
+head = ListNode(1,ListNode(2,ListNode(3,ListNode(4,ListNode(5,ListNode(6,ListNode(7)))))))
+a = Solution().removeNthFromEnd(head,7)
+
+while a:
+    print(a.val)
+    a = a.next
+
